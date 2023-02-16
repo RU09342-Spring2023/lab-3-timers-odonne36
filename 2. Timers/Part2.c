fec6b23b0031f2683226be8dda11d1e6a3ed9728
odonne36 <odonne36@students.rowan.edu>
2023-02-16 16:44:44 -0500

    part 2 complete

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -12,6 +12,7 @@
 void gpioInit();
 void timerInit();
 
+unsigned int t = 50000;
 void main(){
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
@@ -30,15 +31,21 @@ void main(){
 
 void gpioInit(){
     // @TODO Initialize the Red or Green LED
-
+        P1DIR |= BIT0;
+        P1OUT &= ~BIT0;
     // @TODO Initialize Button 2.3
-
+        P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+        P2REN |= BIT3;                          // P2.3 pull-up register enable
+        P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+        P2IE |= BIT3;
 
 }
 
 void timerInit(){
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
-
+TB1CCTL0 = CCIE;
+TB1CCR0 = 50000;
+TB1CTL = TBSSEL_1 | MC_2;
 }
 
 
@@ -50,9 +57,16 @@ void timerInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
+    if (t == 50000)
+        t = 25000;
+    else if (t==25000)
+        t= 10000;
+    else if (t== 10000)
+        t=50000;
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-
+        P2IFG &= ~BIT3;
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
+
 
 }
 
@@ -62,6 +76,8 @@ __interrupt void Port_2(void)
 __interrupt void Timer1_B0_ISR(void)
 {
     // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P1OUT ^= BIT0;
+    TB1CCR0 += t;
 }
 
 
