db26ed87166a6e78a7b179f826e5828692aa2e6b
odonne36 <odonne36@students.rowan.edu>
2023-02-19 19:30:41 -0500

    added some functionality to part 3

--- 3. Design Challenge Button Based Delay/Part3.c
+++ 3. Design Challenge Button Based Delay/Part3.c
@@ -8,13 +8,14 @@
  */
 
 
-#include<msp430.h>
+#include <msp430.h>
 
+unsigned int t = 25000;
+unsigned int led = 0;
+void main(){
 
-int main(void){
-
-WDTCTL = WDTPW | WDTHOLD;
-__bis_SR_register(LPM3_bits | GIE);
+WDTCTL = WDTPW | WDTHOLD;                       //Disable watchdog
+__bis_SR_register(GIE);
 PM5CTL0 &= ~LOCKLPM5;
 
 
@@ -25,11 +26,14 @@ P1OUT &= ~BIT0;
         P2REN |= BIT3;
         P2IES &= ~BIT3;                         // P2.3 Low --> High edge
         P2IE |= BIT3;
+        P2IFG &= ~BIT3;
+     while (1) {
 
         TB1CCTL0 = CCIE;
-        TB1CCR0 = 25000;
+        TB1CCR0 = t;
         TB1CTL = TBSSEL_1 | MC_2;
 
+     }
 
 }
 
@@ -40,12 +44,42 @@ __interrupt void Timer1_B0_ISR(void)
 
 
     P1OUT ^= BIT0;
-    TB1CCR0 += 25000;
+    TB1CCR0 += t;
 }
 
 
 
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
+{
 
+P2IFG &= ~BIT3;
+
+if (!led) {
+
+    led = 1;
+    P2IES ^= BIT3;
+    TB1CTL = TBSSEL_1 | MC_1;
+
+   }
+
+if (led) {
+
+    P2IES ^= BIT3;
+     led = 0;
+     TB1CTL = TBSSEL_1 | MC_0;
+     t = TB1R;
+     TB1CTL = TBSSEL_1 | MC_2;
+
+   }
+
+
+
+
+
+
+
+}
 
 
 
